b4ab907855cc2d047e735e83a6d711e538e8f19c
Paul Salamone <paul.salamone@db-n.com>
2022-09-27 21:15:26 +0200

    first commit

---
+++ nuxt3-sanity/notes-app/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>app page</h1>
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
---
+++ nuxt3-sanity/notes-app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <h2>index</h2>
+    </NuxtLayout>
+  </div>
+</template>
